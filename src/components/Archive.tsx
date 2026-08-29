fc7c3c71f99ea0c3b75353b9ab0e2e76515d1f90
lyriel le <70788262+llyriell@users.noreply.github.com>
2026-08-29 05:48:26 -0400

    Update Archive.tsx

--- src/components/Archive.tsx
+++ src/components/Archive.tsx
@@ -39,12 +39,12 @@ const CODE_ENDINGS: CodeEnding[] = [
     codes: ['home', 'home.'],
     segments: [
   /*
+      { text: 'The door is gone.', pauseAfter: 2000 },
+  */
       { text: 'I see you,', pauseAfter: 1100 },
       { text: ' you are home.', pauseAfter: 2400 },
-  */
-      { text: 'The door is gone.', pauseAfter: 2000 },
     ],
-    unlock: false,
+    unlock: true,
   },
   {
     codes: ['lyriel'],
